75b4a6d3a4869b37e256ef5e0bf569766b5be175
RizWaaN3024 <rizwaanans786@gmail.com>
2025-05-23 21:08:17 +0530

    900

--- 900/A_Even_Odds.java
+++ 900/A_Even_Odds.java
@@ -5,20 +5,19 @@ public class A_Even_Odds
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int arr[] = new int[n];
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+        if (k < 1 || k > n) {
+            return;
+        }
         
-        int pos = 0;
-        for (int i = 1; i <= n; i += 2) {
-            arr[pos] = i;
-            pos++;
-        }
-        for (int i = 2; i <= n; i += 2) {
-            arr[pos] = i;
-            pos++;
-        }
+        long oddCount = (n + 1) / 2;
 
-        System.out.println(arr[k - 1]);
+        if (k <= oddCount) {
+            System.out.println(2 * k - 1);
+        } else {
+            long evenPosition = k - oddCount;
+            System.out.println(2 * evenPosition);
+        }
     }
 }
